ae388cd0b8c07f0751bb461b16c4d41a3924f892
josh <joshtagliere@hotmail.com>
2026-02-27 22:54:04 -0500

    Stop tracking files ignored by gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-model_path = r"C:\Users\joshu\Documents\projects\idk_man_the_fucking_shotput_coach_thing\pose_landmarker_heavy.task"
-
-image_path = ""
-annotated_image_path = ""
-video_path = ""
-annotated_video_path = ""
-annotated_livestream_path = ""
-recording_path = ""
-remember = False
