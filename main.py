31395ed46554091c3d987e23d12cf5bacad866be
josh <joshtagliere@hotmail.com>
2026-04-18 21:24:53 -0400

    removed qt/pyside in favor of threading and queue, no local gui anymore, and implemented a basic api for video upload and retrieval

--- main.py
+++ main.py
@@ -1,109 +1,70 @@
-
-from collections import deque
-
 import processing
-from PySide6.QtCore import Slot, Signal, QThread, QObject
-from PySide6.QtWidgets import QApplication
+import threading
+import queue
 import cv2 as cv
-import sys
-import numpy as np
-import gui
+import os
+import subprocess
 
-class Main(QObject):
-    data = Signal(cv.VideoCapture)
-    frame_transfer = Signal(np.ndarray, int)
-    img_path = Signal(str)
-    def __init__(self):
-        super().__init__()
-        self.process_type = ""
-        
-        self.cap = None
-        
-    def fetch(self, mode, file, output, save_annotated, save_raw, raw_stream):
-        self.process_type = mode
+class Main():
+    def __init__(self, file, output):
         self.path = file
-        if save_annotated:
-            self.output = output
-        if save_raw:
-            self.raw_stream = raw_stream
-        
-        
-    @Slot(str)
-    def set_process(self, process_type: str):
-        self.process_type = process_type
-        
-        
+        self.output = output
+
     def run(self):
-        self.gui = gui.GUI(self)
-        self.gui.show()
+        self.start_processing_threads()
    
     def start_processing_threads(self):
-        self.worker = processing.process(self.process_type)
-        print(self.process_type)
-        if self.process_type != "image":
-            self.capture_worker = Capture(self.process_type, self.path)
-            self.capture_worker.total_frames.connect(self.gui.processing_screen.set_total_frames)
-            self.camera_thread = QThread()
-        else:
-            self.gui.input_screen.image_path.connect(self.worker.get_img_path)
-        self.worker_thread = QThread()
+        self.worker = processing.process(self.path)
         
-        self.worker.moveToThread(self.worker_thread)
+        self.capture_worker = Capture(self.path, self.worker)
+        self.capture_worker.start()
+        self.worker.start()
+        self.worker.join()
+        self.capture_worker.join()
         
-        if self.process_type != "image":
-            self.capture_worker.moveToThread(self.camera_thread)
-            self.capture_worker.capture_signal.connect(self.worker.new_frame)
-            self.capture_worker.fps.connect(self.worker.update_fps)
-            
-        self.worker.image.connect(self.gui.processing_screen.update_buffer)
+        output_path = os.path.join(self.output, 'output.mp4')
+        temp_path = os.path.join(self.output, "temp.mp4")
         
-            
-        self.worker_thread.started.connect(self.worker.run)
-        if self.process_type != "image":
-            self.camera_thread.started.connect(self.capture_worker.run)
+        fourcc = cv.VideoWriter.fourcc(*'mp4v')
+        writer = cv.VideoWriter(temp_path, fourcc, self.capture_worker.fps, self.worker.output_dimensions)
+       
         
-            self.camera_thread.start()
-        self.worker_thread.start()
-        print("threads started")   
-        
-    @Slot(np.ndarray, int)
-    def  captureFrame(self):
-        self.worker.image.connect(self.updateFrame)
-    
-    def updateFrame(self, data: np.ndarray, timestamp: int):
-        self.frame_transfer.emit(data, timestamp)   
 
-class Capture(QThread):
-    capture_signal = Signal(bool, np.ndarray, int)
-    fps = Signal(int)
-    total_frames = Signal(int)
-    running = Signal(bool)
-    seek_signal = Signal(int)
-    
-    def __init__(self, mode: str, video_path: str):
-        super().__init__()
+        while not self.worker.output_queue.empty():
+            self.frame, self.timestamp, self.wall_clock = self.worker.output_queue.get()
+
+            writer.write(self.frame)
+
+        writer.release()
         
+        subprocess.run([
+            "ffmpeg", "-i", temp_path,
+            "-vcodec", "libx264",
+            "-f", "mp4",
+            output_path
+        ])
+        
+        os.remove(os.path.join(temp_path, "temp.mp4"))
+        
+class Capture(threading.Thread):
+    
+    def __init__(self, video_path, worker, mode="video"):
+        super().__init__()
+        self.frame_queue = queue.Queue()
         self.mode = mode
         self.video_path = video_path
         self.cap = None
+        self.worker = worker
 
         if self.mode == "video":
             self.cap = cv.VideoCapture(self.video_path)
-            total = int(self.cap.get(cv.CAP_PROP_FRAME_COUNT))
-            self.total_frames.emit(total)
             
         elif self.mode == "livestream":
             self.cap = cv.VideoCapture(0)
-        if self.cap:    
-            self.seek_signal.connect(self.seek)
-    
-    @Slot(int)
-    def seek(self, frame_number):
-        self.pending_seek = frame_number
-    
-    @Slot()
+
+            
     def run(self):
-        self.pending_seek = None
+        
         if self.cap is None:
             return
 
@@ -111,33 +72,17 @@ class Capture(QThread):
             print(f"Failed to open capture: {self.video_path or 'camera'}")
             return
         
-        self.frame_buffer = deque(maxlen=50000)
-        
-        total = int(self.cap.get(cv.CAP_PROP_FRAME_COUNT))
-        self.total_frames.emit(total)
-        self.running.emit(True)
-        
-        current_frame = 0
-        
+
+        self.fps = int(self.cap.get(cv.CAP_PROP_FPS))
         while self.cap.isOpened():
-            fps = int(self.cap.get(cv.CAP_PROP_FPS))
-            self.fps.emit(fps)
-            
-            if self.pending_seek is not None:
-                self.cap.set(cv.CAP_PROP_POS_FRAMES, self.pending_seek)
-                current_frame = self.pending_seek
-                self.pending_seek = None
                 
             ret, frame = self.cap.read()
-            if ret:
-                current_frame += 1
-                self.frame_buffer.append(frame)
-                self.capture_signal.emit(ret, frame, current_frame)
-
+            if not ret:
+                break
+            self.worker.new_frame(ret, frame, self.fps)
+                
+        self.worker.frame_queue.put(None)
     
 if __name__ == "__main__":
-    qapp = QApplication(sys.argv)
-    main = Main()
+    main = Main(r"C:\Users\joshu\Videos\shotput\output.mp4", r"C:\Users\joshu\Videos\shotput")
     main.run()
-    
-    sys.exit(qapp.exec())
--- processing.py
+++ processing.py
@@ -1,7 +1,5 @@
 import time
-
-import gui
-from PySide6.QtCore import QTimer, Signal, Slot, QObject, QEventLoop, QThread
+import threading
 from mediapipe.tasks.python.components.containers import NormalizedLandmark
 from mediapipe.tasks.python.vision import drawing_utils
 from mediapipe.tasks.python.vision import drawing_styles
@@ -10,7 +8,7 @@ import numpy as np
 import mediapipe as mp
 import cv2 as cv
 import kalman_smooth as ks
-from collections import deque
+import queue
 
 
 ###Mix batch processing, thread pooling, and an initial 5 second buffer.
@@ -26,57 +24,23 @@ BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
-options = None
 
 
-class process(QThread):
-    cap = Signal(cv.VideoCapture)
-    image = Signal(np.ndarray, int)
-    def __init__(self, process_type):
+class process(threading.Thread):
+    def __init__(self, path: str):
         super().__init__()
-        self.process_type = process_type
-        self.received_new_frame = False
+        self.path = path
         self.running = True
-        self.ret = False
         self.frame = None
-        self.result = PoseLandmarker
+        self.result = None
         self.smoothed_landmarks = []
-        self.CAM_FPS = 0
         self.filters = [None]
-        self.running_mode = None
-        self.options = PoseLandmarkerOptions(None)
-        self.model = r"C:\Users\joshu\Documents\projects\idk_man_the_fucking_shotput_coach_thing\pose_landmarker_heavy.task"
-        self.frame_queue = deque(maxlen=2)
-        self.x = -1
-
-    @Slot(str)
-    def route(self, process_type: str):
-        match process_type.lower():
-
-            case "image":
-                self.VisionRunningMode = mp.tasks.vision.RunningMode.IMAGE
-                self.options = PoseLandmarkerOptions(
-                    base_options = BaseOptions(model_asset_path = self.model),
-                    running_mode=self.VisionRunningMode.IMAGE
-                )
-                self.img_path = ""
-                self.x = 0
-                
-            case "video":
-                self.VisionRunningMode = mp.tasks.vision.RunningMode.VIDEO
-                self.options = PoseLandmarkerOptions(
-                    base_options = BaseOptions(model_asset_path=self.model),
-                    running_mode=self.VisionRunningMode.VIDEO)
-                self.x = 1
-                
-            case "livestream":
-                self.VisionRunningMode = mp.tasks.vision.RunningMode.VIDEO
-                self.options = PoseLandmarkerOptions(
-                    base_options = BaseOptions(model_asset_path=self.model),
-                    running_mode=self.VisionRunningMode.VIDEO,
-                )
-                self.x = 2
-    
+        self.model = r"C:\Users\joshu\Documents\projects\shotput_coach\pose_landmarker_heavy.task"
+        self.options = PoseLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=self.model),
+        running_mode=mp.tasks.vision.RunningMode.VIDEO)
+        self.frame_queue = queue.Queue()
+        self.output_queue = queue.Queue()
 
     def draw_landmarks_on_image(self, rgb_image, landmark_list):
 
@@ -95,96 +59,70 @@ class process(QThread):
 
         return annotated_image
 
-    @Slot(bool, np.ndarray)
-    def new_frame(self, ret, frame):
-        
-        self.ret = ret
-
-        if ret and frame is not None:
-            self.frame_queue.append(frame)
-
-    @Slot(int)
-    def update_fps(self, fps):
+    def new_frame(self, ret, frame, fps):
         self.CAM_FPS = fps
 
-    @Slot(str)
-    def get_img_path(self, path: str):
-        self.img_path = path
-        
+        if ret and frame is not None:
+            self.frame_queue.put(frame)
+    
+    def stop(self):
+        self.running = False
         
     def run(self):
         self.timestamp = 0
         try:
-            self.route(self.process_type)
             self.landmarker = PoseLandmarker.create_from_options(self.options)
         except AttributeError as e:
             print(f"Error in run: {e}")
             return
 
         self.filters = [None]
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.process_one_frame)
-        self.timer.start(16)
+        
+        while self.process_one_frame():
+            pass
         
     def process_one_frame(self):
-
-        if self.x == 0:
-            self.timer = None
-            mp_image = mp.Image.create_from_file(self.img_path)
-            
-            pose_landmarker_result = self.landmarker.detect(mp_image)
-            
-            np_image = np.array(mp_image.numpy_view(), dtype=np.uint8)
-            np_image = cv.cvtColor(np_image, cv.COLOR_RGB2BGR)
-            annotated_image = self.draw_landmarks_on_image(np_image, pose_landmarker_result.pose_landmarks)
-            
-            self.image.emit(annotated_image, 1)
-            
-            
-            return
-        else:
-            
         
-            if not self.frame_queue:
-                return
-            self.frame = self.frame_queue.pop()
+        self.frame = self.frame_queue.get()
+        if self.frame is None:
+            return False
+
+        height, width = self.frame.shape[:2]
+        target = (1920,1080) if width > height else (1080,1920)
+
+        if (width, height) != target:
+            self.frame = cv.resize(self.frame, target)
+
+        self.output_dimensions = (width, height)
+        
+        rgb_frame = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        self.timestamp = time.perf_counter_ns() // 10000000
+
+        
+        result = self.landmarker.detect_for_video(mp_image, self.timestamp)
             
+        if not result.pose_landmarks:
+            return self.running
+                
+        self.result = result.pose_landmarks[0]
+
+        if self.result and len(self.result) > 0:
+                
+            if self.filters[0] is None:
+                self.filters = ks.make_filters(self.result, self.CAM_FPS)
+
+            landmarks = self.result
+            smoothed = ks.smooth_landmarks(landmarks, self.filters)
+                
+            self.smoothed_landmarks = [
+                NormalizedLandmark(x=f.x[0], y=f.x[1], z=f.x[2], visibility=landmarks[i].visibility)
+                for i, f in enumerate(smoothed)
+            ]
+
+        annotated_frame = self.draw_landmarks_on_image(self.frame, [self.smoothed_landmarks])
             
-            if not self.ret or self.frame is None:
-                return
+        world_time = time.time()
+        self.output_queue.put((annotated_frame, self.timestamp, world_time))
 
-
-            height, width = self.frame.shape[:2]
-            target = (1920,1080) if width > height else (1080,1920)
-
-            if (width, height) != target:
-                self.frame = cv.resize(self.frame, target)
-
-            rgb_frame = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
-            self.timestamp = time.perf_counter_ns() // 10000000
-
-            if self.x == 1:
-                result = self.landmarker.detect_for_video(mp_image, self.timestamp)
-                if not result.pose_landmarks:
-                    self.received_new_frame = False
-                    return
-                self.result = result.pose_landmarks[0]
-
-            if self.result and len(self.result) > 0:
-                if self.filters[0] is None:
-                    self.filters = ks.make_filters(self.result, self.CAM_FPS)
-
-                landmarks = self.result
-                smoothed = ks.smooth_landmarks(landmarks, self.filters)
-                self.smoothed_landmarks = [
-                    NormalizedLandmark(x=f.x[0], y=f.x[1], z=f.x[2], visibility=landmarks[i].visibility)
-                    for i, f in enumerate(smoothed)
-                ]
-
-            annotated_frame = self.draw_landmarks_on_image(self.frame, [self.smoothed_landmarks])
-            
-
-            
-            self.image.emit(annotated_frame, self.timestamp)
-            self.received_new_frame = False
+        return self.running
